f5339588c4556e129bddb57f2faabb2a6e6e8a1d
darrus baugh <darrusbaugh24@gmail.com>
2018-10-27 15:14:26 -0400

    initial commit

---
+++ first day SQL.sql
@@ -0,0 +1,12 @@
+select * from product;
+select * from product
+where id = 8;
+select * from product where listprice < 55;
+select * from product order by code;
+select * from product order by listprice;
+
+insert into product
+values ('.net', 'appletree','54.50');
+insert into product 
+values ('angular', 'longbottom', '54.50');
+
